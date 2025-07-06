05456994859609141739e4111731652548b02388
ulyweb <uly@ulynetwork.com>
2025-07-06 13:50:53 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,16 +13,12 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
