e59cf64fd7ba85345a080bdcdf0b2af36e36902e
ulyweb <uly@ulynetwork.com>
2025-07-06 13:24:47 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'fasting-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
